6d406d673a5433aa8de639cf9435a68df4d2a68f
SmartForger <23446619+SmartForger@users.noreply.github.com>
2019-08-30 18:34:16 -0700

    handle canvas resize

--- src/views/Canvas.js
+++ src/views/Canvas.js
@@ -11,9 +11,23 @@ class Canvas extends Component {
   }
 
   componentDidMount() {
+    const { setCanvas, webComponent } = this.props;
     this.fitToContainer(this.canvasRef.current);
     this.canvas = new fabric.Canvas(this.canvasRef.current);
-    this.props.setCanvas(this.canvas);
+    setCanvas(this.canvas);
+
+    if (webComponent) {
+      webComponent.dispatchEvent(new CustomEvent("onResizeCallback", {
+        detail: {
+          callback: () => {
+            const canvasEl = this.canvasRef.current;
+            this.canvas.setWidth(canvasEl.clientWidth);
+            this.canvas.setHeight(canvasEl.clientHeight);
+            this.canvas.renderAll();
+          }
+        }
+      }))
+    }
   }
 
   fitToContainer(canvas) {
@@ -38,12 +52,16 @@ class Canvas extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  webComponent: state.component.component
+});
+
 const mapDispatchToProps = dispatch => ({
   setCanvas: canvas => dispatch(setCanvas(canvas)),
   deleteObject: () => dispatch(deleteObject())
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(Canvas);
